7fc43f51c5cfd3e8ee20071881325ec88779e479
BartugKaan <bartugkaan.celebi@outlook.com>
2023-07-03 14:12:27 +0300

    Added : Initial Files

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Bartuğ Kaan Çelebi on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
