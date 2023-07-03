d6d119b7e6516feb82fd778ddc1cbee1a244684a
BartugKaan <bartugkaan.celebi@outlook.com>
2023-07-03 15:49:33 +0300

    Added : CheckoutView

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,35 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Bartuğ Kaan Çelebi on 3.07.2023.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    @EnvironmentObject var order: Order
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    @State private var paymentType = "Cash"
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView().environmentObject(Order())
+    }
+}
